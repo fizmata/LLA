8840d7b7b24159082bc70fc0e3722e4e5a10e3d1
Giorgi Zeikidze <gio.zeikidze@gmail.com>
2023-04-27 00:01:19 +0300

    add random GUI element

--- gui.py
+++ gui.py
@@ -4,4 +4,11 @@ class MyApp(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         # Define your GUI widgets here
-        self.pack()
+        # self.pack()
+        master.title('LLApp')
+        
+        random_label = tk.Label(master, text='Random')
+        random_label.grid(row=0, column=0, padx=5, pady=5)
+        random_entry = tk.Entry(master)
+        random_entry.grid(row=0, column=1, padx=5, pady=5)
+
