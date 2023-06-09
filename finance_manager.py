ea75e2430c2b05441bab316b5c4390ada3cc3428
Fathin Dosunmu <fathin@zenown.com>
2023-06-09 14:13:18 +0300

    added comments to the code

--- finance_manager.py
+++ finance_manager.py
@@ -2,20 +2,24 @@ import csv
 import os
 from collections import defaultdict
 
+
 class PersonalFinanceManager:
     def __init__(self, file_name="personal_finance.csv"):
         self.file_name = file_name
+        # Create a CSV file if it doesn't exist
         if not os.path.isfile(self.file_name):
             with open(self.file_name, mode='w') as file:
                 writer = csv.writer(file)
                 writer.writerow(["Type", "Category", "Amount", "Date"])
 
     def add_entry(self, entry_type, category, amount, date):
+        # Append an entry to the CSV file
         with open(self.file_name, mode='a') as file:
             writer = csv.writer(file)
             writer.writerow([entry_type, category, amount, date])
 
     def show_finance_status(self):
+        # Calculate total income, total expense, and balance
         total_income = 0
         total_expense = 0
         with open(self.file_name, 'r') as read_obj:
@@ -25,25 +29,26 @@ class PersonalFinanceManager:
                     total_income += float(line[2])
                 elif line[0].lower() == "expense":
                     total_expense += float(line[2])
-
         return total_income, total_expense, total_income - total_expense
 
     def show_spending_insights(self):
+        # Calculate spending by category
         expense_by_category = defaultdict(float)
         with open(self.file_name, 'r') as read_obj:
             csv_reader = csv.reader(read_obj)
             for line in csv_reader:
                 if line[0].lower() == "expense":
                     expense_by_category[line[1]] += float(line[2])
-
         return expense_by_category
-    
+
     def set_budget(self, category, budget):
+        # Write the budget to the CSV file
         with open('budget.csv', mode='a') as file:
             writer = csv.writer(file)
             writer.writerow([category, budget])
 
     def check_budget(self, category, amount):
+        # Check if the expense is within the budget
         with open('budget.csv', 'r') as file:
             reader = csv.reader(file)
             for row in reader:
--- gui.py
+++ gui.py
@@ -2,24 +2,30 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLi
 from PyQt5.QtCore import QDate
 from finance_manager import PersonalFinanceManager
 
-
 class BudgetDialog(QDialog):
     def __init__(self, finance_manager):
         super().__init__()
         self.finance_manager = finance_manager
         self.setWindowTitle("Set Budget")
         self.setLayout(QVBoxLayout())
+
+        # Budget input fields
         self.category_input = QLineEdit()
         self.budget_input = QLineEdit()
+
+        # Adding input fields to the layout
         self.layout().addWidget(QLabel("Category"))
         self.layout().addWidget(self.category_input)
         self.layout().addWidget(QLabel("Budget"))
         self.layout().addWidget(self.budget_input)
+
+        # Set budget button
         self.button = QPushButton("Set Budget")
         self.button.clicked.connect(self.set_budget)
         self.layout().addWidget(self.button)
 
     def set_budget(self):
+        # When the button is clicked, set the budget using the finance manager
         category = self.category_input.text()
         budget = float(self.budget_input.text())
         self.finance_manager.set_budget(category, budget)
@@ -31,49 +37,54 @@ class FinanceApp(QWidget):
         super().__init__()
 
         self.finance_manager = finance_manager
-
         self.layout = QVBoxLayout()
 
+        # Income/Expense input field
         self.entry_type_input = QLineEdit()
         self.layout.addWidget(QLabel("Type (Income/Expense):"))
         self.layout.addWidget(self.entry_type_input)
 
+        # Category input field
         self.category_input = QLineEdit()
         self.layout.addWidget(QLabel("Category:"))
         self.layout.addWidget(self.category_input)
 
+        # Amount input field
         self.amount_input = QLineEdit()
         self.layout.addWidget(QLabel("Amount:"))
         self.layout.addWidget(self.amount_input)
 
+        # Date input field
         self.date_input = QDateEdit()
         self.date_input.setDisplayFormat("yyyy-MM-dd")
         self.date_input.setDate(QDate.currentDate())
         self.layout.addWidget(QLabel("Date (YYYY-MM-DD):"))
         self.layout.addWidget(self.date_input)
 
+        # Add Entry button
         self.add_entry_button = QPushButton("Add Entry")
         self.add_entry_button.clicked.connect(self.add_entry)
         self.layout.addWidget(self.add_entry_button)
 
+        # Show Finance Status button
         self.show_finance_status_button = QPushButton("Show Finance Status")
-        self.show_finance_status_button.clicked.connect(
-            self.show_finance_status)
+        self.show_finance_status_button.clicked.connect(self.show_finance_status)
         self.layout.addWidget(self.show_finance_status_button)
 
-        self.show_spending_insights_button = QPushButton(
-            "Show Spending Insights")
-        self.show_spending_insights_button.clicked.connect(
-            self.show_spending_insights)
+        # Show Spending Insights button
+        self.show_spending_insights_button = QPushButton("Show Spending Insights")
+        self.show_spending_insights_button.clicked.connect(self.show_spending_insights)
         self.layout.addWidget(self.show_spending_insights_button)
 
-        self.setLayout(self.layout)
-
+        # Set Budget button
         self.set_budget_button = QPushButton("Set Budget")
         self.set_budget_button.clicked.connect(self.set_budget)
         self.layout.addWidget(self.set_budget_button)
 
+        self.setLayout(self.layout)
+
     def add_entry(self):
+        # When the button is clicked, add the entry using the finance manager
         try:
             self.finance_manager.add_entry(
                 self.entry_type_input.text(),
@@ -81,29 +92,29 @@ class FinanceApp(QWidget):
                 float(self.amount_input.text()),
                 self.date_input.date().toString("yyyy-MM-dd")
             )
-            QMessageBox.information(
-                self, "Success", "Entry added successfully!")
+            QMessageBox.information(self, "Success", "Entry added successfully!")
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
     def show_finance_status(self):
+        # When the button is clicked, show the finance status using the finance manager
         try:
             income, expense, balance = self.finance_manager.show_finance_status()
-            QMessageBox.information(
-                self, "Finance Status", f"Total Income: {income}\nTotal Expense: {expense}\nBalance: {balance}")
+            QMessageBox.information(self, "Finance Status", f"Total Income: {income}\nTotal Expense: {expense}\nBalance: {balance}")
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
     def show_spending_insights(self):
+        # When the button is clicked, show the spending insights using the finance manager
         try:
             insights = self.finance_manager.show_spending_insights()
-            insights_str = "\n".join(
-                [f"{category}: {amount}" for category, amount in insights.items()])
+            insights_str = "\n".join([f"{category}: {amount}" for category, amount in insights.items()])
             QMessageBox.information(self, "Spending Insights", insights_str)
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
     def set_budget(self):
+        # When the button is clicked, show the budget dialog
         self.budget_dialog = BudgetDialog(self.finance_manager)
         self.budget_dialog.show()
 
@@ -111,6 +122,7 @@ class FinanceApp(QWidget):
 if __name__ == "__main__":
     app = QApplication([])
 
+    # Create a finance manager and the GUI
     finance_manager = PersonalFinanceManager()
     gui = FinanceApp(finance_manager)
     gui.show()
